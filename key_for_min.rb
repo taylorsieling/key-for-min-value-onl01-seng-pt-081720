e45c5b46736b8d9260101396c6d6d8a54e63c3a2
Taylor Sieling <taylorasieling@gmail.com>
2020-08-29 02:49:02 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,14 +2,14 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  comparitive_value == nil #?
-  comparitive_key == nil #?
+  comparitive_value == 0 
+  comparitive_key == nil
   
   #set values to compare the hash to
   #next start the interation
   
   name_hash.each do |key, value|
-    if comparitive_value = nil
+    if comparitive_value = 0 
       comparitive_value = value
       comparitive_key = key
     end
