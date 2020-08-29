3bdd617c4ea9eac38b3e0ce210701aa02d5cd923
Taylor Sieling <taylorasieling@gmail.com>
2020-08-29 02:56:31 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  lowest_value == 0 
-  lowest_key == nil
+  lowest_value = nil 
+  lowest_key = nil
   
   #set current minimum to compare the hash to
   #next start the interation
